995e9293aeb38b5604506312310b8ea537d3523c
Martacus <mcpkmart@hotmail.nl>
2024-04-01 19:36:56 +0200

    Changed from rethinkdb to mongodb

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.22.1
 
 require (
 	github.com/gin-gonic/gin v1.9.1
-	gopkg.in/rethinkdb/rethinkdb-go.v6 v6.2.2
+	github.com/joho/godotenv v1.5.1
+	go.mongodb.org/mongo-driver v1.14.0
 )
 
 require (
@@ -16,27 +17,30 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/golang/protobuf v1.5.0 // indirect
-	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
-	gopkg.in/cenkalti/backoff.v2 v2.2.1 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,183 +1,118 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
-	rethink "gopkg.in/rethinkdb/rethinkdb-go.v6"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type JsonLog struct {
-	Domain string   `json:"domain"`
-	Path   string   `json:"path"`
-	Tags   []string `json:"tags"`
-	Log    string   `json:"log"`
+	Domain    string `json:"domain"`
+	Group     string `json:"group"`
+	Tag       string `json:"tag"`
+	Log       string `json:"log"`
+	Timestamp int64  `json:"timestamp"`
 }
 
-var dbName = "lw_logger"
-
 func main() {
-	// Connect to RethinkDB server
-	session, rethinkErr := rethink.Connect(rethink.ConnectOpts{
-		Address: "localhost:28015", // default
-	})
-	if rethinkErr != nil {
-		log.Fatalln(rethinkErr)
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
 	}
 
-	cdbErr := createDatabase(session)
-	if cdbErr != nil {
-		log.Fatalln(cdbErr)
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
 
-	ctbErr := createTableIfNotExists(session, "logs")
-	if ctbErr != nil {
-		log.Fatalln(ctbErr)
+	dbName := os.Getenv("MONGODB_DB")
+	if dbName == "" {
+		log.Fatal("You must set your 'dbName' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
+
+	client, mongoErr := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if mongoErr != nil {
+		panic(mongoErr)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	database := client.Database(dbName)
 
 	//Gin Server
 	gServer := gin.Default()
 
-	// ET
-	gServer.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
 	gServer.GET("/log/:domain", func(c *gin.Context) {
 		domain := c.Param("domain")
+		coll := database.Collection(domain)
+
 		var results []JsonLog
-		res, err := rethink.DB(dbName).Table(domain).Run(session)
+		cur, err := coll.Find(context.TODO(), bson.D{{}})
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			panic(err)
 		}
 
-		if err = res.All(&results); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
+		curAllErr := cur.All(context.TODO(), &results)
+		if curAllErr != nil {
+			panic(curAllErr)
 		}
 
 		c.JSON(200, results)
 	})
 
-	//
+	gServer.GET("/log/:domain/group/list", func(c *gin.Context) {
+		domain := c.Param("domain")
+		coll := database.Collection(domain)
+
+		res, err := coll.Distinct(context.TODO(), "group", bson.D{{}})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		log.Println(len(res))
+
+		c.JSON(200, res)
+	})
+
+	gServer.GET("/domain/list", func(c *gin.Context) {
+		collections, err := database.ListCollectionNames(context.TODO(), bson.D{{}})
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(200, collections)
+	})
+
 	gServer.POST("/log", func(c *gin.Context) {
 		var logEntry JsonLog
 		if err := c.BindJSON(&logEntry); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+		logEntry.Timestamp = time.Now().UTC().UnixMilli()
 
-		if err := createTableIfNotExists(session, logEntry.Domain); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		err := rethink.DB(dbName).Table(logEntry.Domain).Insert(logEntry).Exec(session)
+		coll := database.Collection(logEntry.Domain)
+		result, err := coll.InsertOne(context.TODO(), logEntry)
 		if err != nil {
 			log.Println("Failed to insert logEntry:", err)
 		}
 
 		log.Printf("Log inserted %s\n", logEntry.Log)
+		c.JSON(200, result)
 	})
+
 	err := gServer.Run()
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-}
-
-func createTableIfNotExists(session *rethink.Session, tbName string) error {
-	existingTables, err := checkTablesExists(session, tbName)
-	if err != nil {
-		log.Fatalln(err)
-		return err
-	}
-
-	for tableName, exists := range existingTables {
-		if exists {
-			log.Printf("Table %s exists\n", tableName)
-		} else {
-			if err := createTable(session, tableName); err != nil {
-				log.Fatalln(err)
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-func createTable(session *rethink.Session, tbName string) error {
-	_, err := rethink.DB(dbName).TableCreate(tbName).Run(session)
-	if err != nil {
-		log.Fatalln(err)
-		return err
-	}
-	log.Printf("Table %s created\n", tbName)
-
-	return nil
-}
-
-func checkTablesExists(session *rethink.Session, tableNames ...string) (map[string]bool, error) {
-	res, err := rethink.DB(dbName).TableList().Run(session)
-	if err != nil {
-		return nil, err
-	}
-
-	var tables []string
-	if err := res.All(&tables); err != nil {
-		return nil, err
-	}
-
-	existsMap := make(map[string]bool)
-
-	for _, tableName := range tableNames {
-		existsMap[tableName] = false
-	}
-
-	for _, t := range tables {
-		if _, ok := existsMap[t]; ok {
-			existsMap[t] = true
-		}
-	}
-
-	return existsMap, nil
-}
-
-func createDatabase(session *rethink.Session) error {
-	dbs, err := rethink.DBList().Run(session)
-	if err != nil {
-		log.Fatalf("Error listing databases: %s", err)
-		return err
-	}
-
-	var dbList []string
-	err = dbs.All(&dbList)
-	if err != nil {
-		log.Fatalf("Error reading database list: %s", err)
-		return err
-	}
-
-	dbExists := false
-	for _, db := range dbList {
-		if db == dbName {
-			dbExists = true
-			break
-		}
-	}
-
-	if !dbExists {
-		_, err = rethink.DBCreate(dbName).RunWrite(session)
-		if err != nil {
-			log.Fatalf("Error creating database %s: %s", dbName, err)
-			return err
-		} else {
-			log.Printf("Database %s created successfully.", dbName)
-		}
-	} else {
-		log.Printf("Database %s already exists.", dbName)
-	}
-	return nil
 }
