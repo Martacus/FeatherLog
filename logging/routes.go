a7e2c87d5f42e7cd9f41a7544a7c11bdeb4d3721
Martacus <mcpkmart@hotmail.nl>
2024-04-18 18:25:21 +0200

    Divide the logging part for move to be a application building tool platform

---
+++ logging/routes.go
@@ -0,0 +1,106 @@
+package logging
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+	"time"
+)
+
+type JsonLog struct {
+	Domain    string `json:"domain"`
+	Group     string `json:"group"`
+	Tag       string `json:"tag"`
+	Log       string `json:"log"`
+	Timestamp int64  `json:"timestamp"`
+}
+
+type Domain struct {
+	Domain string `json:"domain"`
+}
+
+func CreateRoutes(engine *gin.Engine, database *mongo.Database) {
+	//Gets all logs for a domain
+	engine.GET("/log/:domain", func(c *gin.Context) {
+		var results []JsonLog
+		domain := c.Param("domain")
+		coll := database.Collection(domain)
+
+		err := executeQueryWithTimeout(func(ctx context.Context) error {
+			opts := options.Find().SetSort(bson.D{{"timestamp", -1}})
+			cur, findErr := coll.Find(ctx, bson.D{{}}, opts)
+			if findErr != nil {
+				return findErr
+			}
+			return cur.All(ctx, &results)
+		})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, results)
+	})
+
+	//Gets a list of domains
+	engine.GET("/domain/list", func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		collections, err := database.ListCollectionNames(ctx, bson.D{{}})
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		domains := make([]Domain, 0)
+		for _, collection := range collections {
+			domains = append(domains, Domain{Domain: collection})
+		}
+
+		c.JSON(http.StatusOK, domains)
+	})
+
+	//Posts a new log to a domain
+	engine.POST("/log", func(c *gin.Context) {
+		var logEntry JsonLog
+		if err := c.BindJSON(&logEntry); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		logEntry.Timestamp = time.Now().UTC().UnixMilli()
+
+		coll := database.Collection(logEntry.Domain)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		result, err := coll.InsertOne(ctx, logEntry)
+		if err != nil {
+			log.Println("Failed to insert logEntry:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		log.Printf("Log inserted %s\n", logEntry.Log)
+		c.JSON(http.StatusOK, result)
+	})
+}
+
+// executeQueryWithTimeout executes a MongoDB operation with a 10-second timeout
+// the function will log and return an error if the operation fails
+// more context may be added in the future
+func executeQueryWithTimeout(op func(ctx context.Context) error) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := op(ctx)
+	if err != nil {
+		log.Printf("MongoDB operation failed: %v", err)
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,47 +1,38 @@
 package main
 
 import (
+	"LowLogBackend/logging"
 	"context"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
-	"net/http"
 	"os"
 	"time"
 )
 
-type JsonLog struct {
-	Domain    string `json:"domain"`
-	Group     string `json:"group"`
-	Tag       string `json:"tag"`
-	Log       string `json:"log"`
-	Timestamp int64  `json:"timestamp"`
-}
+const (
+	MongoDBURIEnvVar    = "MONGODB_URI"
+	MongoDBNameEnvVar   = "MONGODB_DB"
+	MongoDBDocsURL      = "https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable"
+	ErrorEnvVarNotFound = "you must set your '%s' environment variable. See more at %s"
+)
 
-type Domain struct {
-	Domain string `json:"domain"`
+type MongoDatabaseVariables struct {
+	URI    string
+	DBName string
 }
 
 func main() {
-	// Load .env file
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, will try to use run env vars.")
+	dbVars, err := getDatabaseVariables()
+	if err != nil {
+		log.Fatal("Failed to load database variables: ", err)
 	}
 
-	uri := os.Getenv("MONGODB_URI")
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
-	}
-
-	dbName := os.Getenv("MONGODB_DB")
-	if dbName == "" {
-		log.Fatal("You must set your 'dbName' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
-	}
-
-	database := getDatabase(uri, dbName)
+	database := getDatabase(dbVars.URI, dbVars.DBName)
 	defer func() {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
@@ -53,76 +44,32 @@ func main() {
 	//Create a new gin server
 	gServer := gin.Default()
 
-	//Gets all logs for a domain
-	gServer.GET("/log/:domain", func(c *gin.Context) {
-		var results []JsonLog
-		domain := c.Param("domain")
-		coll := database.Collection(domain)
-
-		err := executeQueryWithTimeout(func(ctx context.Context) error {
-			opts := options.Find().SetSort(bson.D{{"timestamp", -1}})
-			cur, findErr := coll.Find(ctx, bson.D{{}}, opts)
-			if findErr != nil {
-				return findErr
-			}
-			return cur.All(ctx, &results)
-		})
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.JSON(http.StatusOK, results)
-	})
-
-	//Gets a list of domains
-	gServer.GET("/domain/list", func(c *gin.Context) {
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		collections, err := database.ListCollectionNames(ctx, bson.D{{}})
-		if err != nil {
-			log.Println(err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		domains := make([]Domain, 0)
-		for _, collection := range collections {
-			domains = append(domains, Domain{Domain: collection})
-		}
-
-		c.JSON(http.StatusOK, domains)
-	})
-
-	//Posts a new log to a domain
-	gServer.POST("/log", func(c *gin.Context) {
-		var logEntry JsonLog
-		if err := c.BindJSON(&logEntry); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		logEntry.Timestamp = time.Now().UTC().UnixMilli()
-
-		coll := database.Collection(logEntry.Domain)
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		result, err := coll.InsertOne(ctx, logEntry)
-		if err != nil {
-			log.Println("Failed to insert logEntry:", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		log.Printf("Log inserted %s\n", logEntry.Log)
-		c.JSON(http.StatusOK, result)
-	})
+	//Setting up routes
+	logging.CreateRoutes(gServer, database)
 
 	//Run server after establishing routes
 	runErr := gServer.Run()
 	if runErr != nil {
 		log.Fatalln(runErr)
 	}
+}
+
+func getDatabaseVariables() (*MongoDatabaseVariables, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, errors.New("no .env file found, will try to use run environment variables")
+	}
+
+	uri := os.Getenv(MongoDBURIEnvVar)
+	if uri == "" {
+		return nil, fmt.Errorf(ErrorEnvVarNotFound, MongoDBURIEnvVar, MongoDBDocsURL)
+	}
+
+	dbName := os.Getenv(MongoDBNameEnvVar)
+	if dbName == "" {
+		return nil, fmt.Errorf(ErrorEnvVarNotFound, MongoDBNameEnvVar, MongoDBDocsURL)
+	}
+
+	return &MongoDatabaseVariables{URI: uri, DBName: dbName}, nil
 }
 
 // getDatabase returns a MongoDB database object
@@ -144,19 +91,4 @@ func getDatabase(uri string, dbName string) *mongo.Database {
 
 	log.Println("Successfully connected and pinged MongoDB.")
 	return client.Database(dbName)
-}
-
-// executeQueryWithTimeout executes a MongoDB operation with a 10-second timeout
-// the function will log and return an error if the operation fails
-// more context may be added in the future
-func executeQueryWithTimeout(op func(ctx context.Context) error) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err := op(ctx)
-	if err != nil {
-		log.Printf("MongoDB operation failed: %v", err)
-		return err
-	}
-	return nil
 }
